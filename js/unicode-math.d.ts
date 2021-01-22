831129748a45b4f82012cf03efbec4514bce9e22
Peter Krautzberger <p.krautzberger@gmail.com>
2021-01-22 18:15:13 +0100

    feat: rename Configuration => unicode-math
    * package: rename main and add build script
    * bin/make_maps: rename output
    * /ts: rename Configuration.ts => unicode-math.ts
    * add CJS build
    
    Resolves #3

---
+++ js/unicode-math.d.ts
@@ -0,0 +1,2 @@
+import { Configuration } from 'mathjax-full/js/input/tex/Configuration.js';
+export declare const configuration: Configuration;
--- ts/unicode-math.ts
+++ ts/unicode-math.ts
@@ -1,73 +1,10 @@
-/*************************************************************
- *  Copyright (c) 2020 Peter Krautzberger
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
-import NodeUtil from 'mathjax-full/js/input/tex/NodeUtil.js';
 import { Configuration } from 'mathjax-full/js/input/tex/Configuration.js';
 import { CommandMap, EnvironmentMap, DelimiterMap } from 'mathjax-full/js/input/tex/SymbolMap.js';
 import { ParseMethod } from 'mathjax-full/js/input/tex/Types.js';
-import { TexConstant } from 'mathjax-full/js/input/tex/TexConstants.js';
 import ParseMethods from 'mathjax-full/js/input/tex/ParseMethods.js';
 import BaseMethods from 'mathjax-full/js/input/tex/base/BaseMethods.js';
-import TexParser from 'mathjax-full/js/input/tex/TexParser';
 
 let Methods: Record<string, ParseMethod> = {};
-
-// EXAMPLE: custom methods from mathjax-ams-macros
-// Methods.accentset = function (parser: TexParser, name: string) {
-//     const accent = parser.ParseArg(name);
-//     const expression = parser.ParseArg(name);
-//     NodeUtil.setAttribute(accent, 'accent', true);
-//     NodeUtil.setAttribute(accent, 'mathsize', 'small');
-//     if (parser.stack.env.font) {
-//         NodeUtil.setAttribute(accent, 'mathvariant', parser.stack.env.font);
-//     }
-//     const mpadded = parser.create('node', 'mpadded', [accent], { height: 0 });
-//     const mover = parser.create('node', 'mover', [expression, mpadded]);
-//     const texatom = parser.create('node', 'TeXAtom', [mover]);
-//     parser.Push(texatom);
-// };
-
-// Methods.smallcaps = function (parser: TexParser, name: string) {
-//     let argument = parser.GetArgument(name);
-//     const def = {
-//         mathsize: '1em',
-//         mathvariant: 'normal', // TODO why does TexConstants.TexConstant.VARIANT.NORMAL throw?
-//     };
-//     for (let char of argument) {
-//         if (char.toLowerCase() === char) {
-//             def.mathsize = '0.8em';
-//         } else def.mathsize = '1em';
-//         parser.Push(parser.create('token', 'mi', def, char.toUpperCase()));
-//     }
-// };
-
-// Methods.bevelledFraction = function (parser: TexParser, name: string) {
-//     const num = parser.ParseArg(name);
-//     const den = parser.ParseArg(name);
-//     const frac = parser.create('node', 'mfrac', [num, den], {
-//         bevelled: true,
-//     });
-//     parser.Push(frac);
-// };
-
-
-// EXAMPLE: Importing MathJax methods
-// Methods.Macro = BaseMethods.Macro;
-// Methods.Accent = BaseMethods.Accent;
-// Methods.SetFont = BaseMethods.SetFont;
 
 new CommandMap(
     'unicode-math-macros',
@@ -2542,16 +2479,13 @@ new EnvironmentMap(
     'unicode-math-environments',
     ParseMethods.environment,
     {
-        // EXAMPLE: environments from mathjax-ams-macros
-        // dcases: ['Array', null, '\\{', '.', 'll', null, '.2em', 'D'],
-        // bsmallmatrix: ['Array', null, '[', ']', 'c', '0.333em', '.2em', 'S', 1],
     },
     {
         Array: BaseMethods.Array,
     }
 );
 
-export const configuration = Configuration.create('ams-macros', {
+export const configuration = Configuration.create('unicode-math', {
     handler: {
         delimiter: ['unicode-math-delimiters'],
         macro: ['unicode-math-macros', 'unicode-math-delimiters'],
