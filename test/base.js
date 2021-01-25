708a12bfff990d7c1db9a72c88eb0fcffff86d3a
Peter Krautzberger <p.krautzberger@gmail.com>
2021-01-25 20:42:23 +0100

    test: switch to unicode-math.html
    * package: stop piping output
    * bin/make_test: replace $...$ with \(...\), remove mathjax scripts
    * test/unicode-math.html: regenerate
    * test/base.js:  read unicode-math.html, write unicode-math-output.html
    * replace test/test.html with test/unicode-math-output.html

--- test/base.js
+++ test/base.js
@@ -1,10 +1,9 @@
+const fs =  require('fs');
 const mj = require('./mj');
 
-const theBigString =
-  '<html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>' +
-  '$$\\mathsemicolon$$' +
-  '$$ \\eqqslantless$$' +
-  '$$\\overbracket x$$' +
-  '</body></html>';
 
-console.log(mj(theBigString, true));
+console.log(__dirname);
+
+const theBigString = fs.readFileSync(__dirname +'/unicode-math.html').toString();
+
+fs.writeFileSync(__dirname+'/unicode-math-output.html', mj(theBigString));
