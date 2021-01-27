eebbea10c883bd39156e793dfcc0e8a3171d2467
Peter Krautzberger <p.krautzberger@gmail.com>
2021-01-27 18:30:17 +0100

    chore: regenerate TS, JS

--- js/unicode-math.js
+++ js/unicode-math.js
@@ -1,4 +1,19 @@
 "use strict";
+/*!
+ *  Copyright (c) 2021 American Mathematical Society
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.configuration = void 0;
 var Configuration_js_1 = require("mathjax-full/js/input/tex/Configuration.js");
