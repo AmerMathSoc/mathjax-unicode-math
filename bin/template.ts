3e0b4e7b91a40ea9bebf182fd33e71ea602cd162
David M. Jones <dmj@ams.org>
2021-01-28 14:57:47 -0500

    fix: Revised definitions of unicode-math characters.
    Moved non-accent commands out of CommandMap and into CharacterMap.
    Added TeX math class information to characters.
    
    Delimiters now work as expected with \left and \right, but there are
    still some glitches with \limits and \nolimits for some (big)
    operators.

--- bin/template.ts
+++ bin/template.ts
@@ -15,10 +15,12 @@
  */
 
 import { Configuration } from 'mathjax-full/js/input/tex/Configuration.js';
-import { CommandMap, EnvironmentMap, DelimiterMap } from 'mathjax-full/js/input/tex/SymbolMap.js';
+import { CommandMap, CharacterMap, DelimiterMap } from 'mathjax-full/js/input/tex/SymbolMap.js';
 import { ParseMethod } from 'mathjax-full/js/input/tex/Types.js';
 import ParseMethods from 'mathjax-full/js/input/tex/ParseMethods.js';
 import BaseMethods from 'mathjax-full/js/input/tex/base/BaseMethods.js';
+
+import { TEXCLASS } from 'mathjax-full/js/core/MmlTree/MmlNode.js';
 
 let unicodeMathMethods: Record<string, ParseMethod> = {};
 
@@ -26,25 +28,6 @@ unicodeMathMethods.Macro = BaseMethods.Macro;
 unicodeMathMethods.Accent = BaseMethods.Accent;
 unicodeMathMethods.UnderOver = BaseMethods.UnderOver;
 unicodeMathMethods.Not = BaseMethods.Not;
-
-new CommandMap(
-    'unicode-math-macros',
-    {
-    },
-    unicodeMathMethods
-);
-
-new DelimiterMap('unicode-math-delimiters', ParseMethods.delimiter, {
-});
-
-new EnvironmentMap(
-    'unicode-math-environments',
-    ParseMethods.environment,
-    {
-    },
-    {
-    }
-);
 
 export const configuration = Configuration.create('unicode-math', {
     handler: {
