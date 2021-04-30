39bd40ce9d3c8c087bd2db1fd692b0198414511f
Peter Krautzberger <p.krautzberger@gmail.com>
2021-04-30 15:22:28 +0200

    chore(base.js): remove console.log

--- test/base.js
+++ test/base.js
@@ -1,9 +1,6 @@
 const fs =  require('fs');
 const mj = require('./mj');
 
-
-console.log(__dirname);
-
 const theBigString = fs.readFileSync(__dirname +'/unicode-math.html').toString();
 
 fs.writeFileSync(__dirname+'/unicode-math-output.html', mj(theBigString));
