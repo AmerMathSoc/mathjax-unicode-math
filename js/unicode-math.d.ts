f920b32aa42f5b6621d858d4db6662636c0f4602
Peter Krautzberger <p.krautzberger@gmail.com>
2022-12-21 16:32:30 +0100

    chore: regenerate tests and distribution

--- js/unicode-math.d.ts
+++ js/unicode-math.d.ts
@@ -13,5 +13,5 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-import { Configuration } from 'mathjax-full/js/input/tex/Configuration.js';
+import { Configuration } from "mathjax-full/js/input/tex/Configuration.js";
 export declare const configuration: Configuration;
