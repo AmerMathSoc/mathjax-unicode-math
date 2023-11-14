897ac0025b6b1725b743a17e38d03a057b51bb66
Peter Krautzberger <p.krautzberger@gmail.com>
2023-11-14 09:39:04 +0100

    chore: regenerate build

--- js/unicode-math.js
+++ js/unicode-math.js
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 import { Configuration } from "mathjax-full/mjs/input/tex/Configuration.js";
-import { CommandMap, CharacterMap, DelimiterMap, } from "mathjax-full/mjs/input/tex/SymbolMap.js";
+import { CommandMap, CharacterMap, DelimiterMap, } from "mathjax-full/mjs/input/tex/TokenMap.js";
 import ParseMethods from "mathjax-full/mjs/input/tex/ParseMethods.js";
 import BaseMethods from "mathjax-full/mjs/input/tex/base/BaseMethods.js";
 import { TEXCLASS } from "mathjax-full/mjs/core/MmlTree/MmlNode.js";
